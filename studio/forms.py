7505f13e3f5c5584572160a4e8b9be5362c59ccc
Ariadna Cortés Pereles <ariadna.cortes@code-ing.com>
2018-03-14 12:36:40 -0700

    sign up, login y logout implementados

--- studio/forms.py
+++ studio/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 from alumnica_model.models import AuthUser, LearnerModel, DataAnalystModel, ContentCreatorModel, AdministratorModel
 
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password=forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = AuthUser
+        fields = ['username','password']
 
 class UserForm(forms.ModelForm):
     password1 = forms.CharField(widget=forms.PasswordInput())
@@ -15,6 +21,10 @@ class LearnerForm(forms.ModelForm):
     class Meta:
         model = LearnerModel
         fields = ['experience_points_field']
+
+    def __init__(self, *args, **kwargs):
+        super(LearnerForm, self).__init__(*args, **kwargs)
+        self.fields['experience_points_field'].required = False
 
 class DataAnalystForm(forms.ModelForm):
     class Meta:
--- studio/views.py
+++ studio/views.py
@@ -1,28 +1,54 @@
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
-from django.forms import ModelForm
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect, render
 from django.views.generic import FormView, RedirectView, DetailView
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 from django.views.generic.base import View
-from studio.forms import UserForm, AdministratorForm, LearnerForm
+
+from alumnica_model.alumnica_entities.users import UserType
+from alumnica_model.models import AuthUser
+from studio.forms import UserForm, AdministratorForm, LearnerForm, ContentCreatorForm, DataAnalystForm, UserLoginForm
 
 
-class LoginView(FormView):
-    form_class = AuthenticationForm
+class LoginView(View):
+    form_class = UserLoginForm
     template_name = "login.html"
     success_url = '/users/profile/'
-    def dispatch(self, request, *args, **kwargs):
+    def get(self,request):
         if request.user.is_authenticated:
-            return HttpResponseRedirect(self.get_success_url())
+            return HttpResponseRedirect('/users/profile')
         else:
-            return super(LoginView, self).dispatch(request, *args, **kwargs)
+            form=UserLoginForm(None)
+            return render(request,"login.html",{'form':form})
 
-    def form_valid(self, form):
-        login(self.request, form.get_user())
-        return super(LoginView, self).form_valid(form)
+    def post(self,request):
+        form=UserLoginForm(data=request.POST)
+        if form.is_valid:
+            username=request.POST['username']
+            password=request.POST['password']
+            try:
+                user=AuthUser.objects.get(username=username)
+                print(username)
+                print(password)
+
+                if user.check_password(password):
+                    login(request,user)
+                    return redirect('/users/profile')
+                else:
+                    print('password didnt match')
+                    form = UserLoginForm(None)
+                    return render(request, "login.html", {'form': form})
+            except AuthUser.DoesNotExist:
+                form = UserLoginForm(None)
+                return render(request, "login.html", {'form': form})
+
+        else:
+            print('form not valid')
+            form = UserLoginForm(None)
+            return render(request, "login.html", {'form': form})
+
+
 
 
 class LogoutView(RedirectView):
@@ -41,8 +67,13 @@ class UserProfile(LoginRequiredMixin, DetailView):
 
 
 class UserSignUp(View):
+    #forms
     user_form_class = UserForm
-    profile_form_class = LearnerForm
+    learner_form_class = LearnerForm
+    admin_form_class = AdministratorForm
+    contentCreator_form_class = ContentCreatorForm
+    dataAnalyst_form_class = DataAnalystForm
+
     template_name = 'signup.html'
 
     def get(self, request):
@@ -51,10 +82,30 @@ class UserSignUp(View):
         # Here I make instances of my form classes and pass them None
         # which tells them that there is no additional data to display (errors, for example)
         user_form = self.user_form_class(None)
-        profile_form = self.profile_form_class(None)
-        print("holi")
+        admin_form = self.admin_form_class(None)
+        learner_form = self.learner_form_class(None)
+        contentCreator_form = self.contentCreator_form_class(None)
+        dataAnalyst_form = self.dataAnalyst_form_class(None)
         # and then just pass them to my template
-        return render(request, self.template_name, {'user_form': user_form, 'profile_form': profile_form})
+        return render(request, self.template_name, {'user_form': user_form, 'admin_form': admin_form, 'learner_form': learner_form,
+                                                    'contentCreator_form': contentCreator_form, 'dataAnalyst_form': dataAnalyst_form})
+
+    def post(self, request, *args, **kwargs):
+        userType=request.POST['user_type']
+        password=request.POST['password1']
+        user_form=UserForm(data=request.POST)
+        user = user_form.save(commit=False)
+        if user_form.is_valid:
+            user.set_password(password)
+            user.save()
+            if userType == UserType.LEARNER:
+                profile=user.profile
+                print(profile)
+                profile.experience_points_field=request.POST['experience_points_field']
+                profile.save()
+            return redirect('/users/login')
+
+        return redirect(request, *args, **kwargs)
 
 
 
