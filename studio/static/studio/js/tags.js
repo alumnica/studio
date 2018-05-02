bff296f2d226bdc02ea701c311f0062e290914b8
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-05-02 18:43:46 -0500

    Add missing oda-tags selectise on tags.js

--- studio/static/studio/js/tags.js
+++ studio/static/studio/js/tags.js
@@ -47,3 +47,34 @@ $('#materias-tags').selectize({
     selectize.setValue(self_tags_selectize)
 }
 });
+
+
+// Tags odas
+
+var odaTags = [
+    {name: 'tag2', value: 't2'},
+    {name: 'tag1', value: 't1'},
+    {name: 'tag3', value: 't3'},
+    {name: 'cosa', value: 'cs'},
+    {name: 'tag5', value: 't5'},
+    {name: 'OTHER', value: 'ot'},
+    {name: 'tag7', value: 't7'},
+];
+
+$('#oda-tags').selectize({
+    labelField: 'name',
+    valueField: 'name',
+    searchField: 'name',
+    hideSelected: true,
+    persist: false,
+    createOnBlur: true,
+    create: function(input) {
+        return {
+            value: input,
+            name: input
+        }
+    },
+    options: odaTags,
+    preload: false,
+    maxItems: 20,
+});
