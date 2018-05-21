a8b36a643f2f691c5483e014b6e711cc0977ce3d
José Luis Valencia Herrera <jose.valencia@code-ing.com>
2018-05-21 17:10:38 -0500

    Modified some URLs' names

--- studio/urls/oda_urls.py
+++ studio/urls/oda_urls.py
@@ -4,11 +4,11 @@ from studio.views.oda_views import ODAsPositionView, ODAsPreviewView, ODAsRedire
     ODAUpdateView, ODACreateView, ODAsSectionView
 
 urlpatterns = [
-    path('odas_section/<int:pk>,<int:section>/', ODAsSectionView.as_view(), name='odas_section_view'),
-    path('odas_position/<int:pk>,<int:section>/', ODAsPositionView.as_view(), name='odas_position_view'),
-    path('odas_preview/<int:pk>/', ODAsPreviewView.as_view(), name='odas_preview_view'),
-    path('odas_back/<slug:view>, <int:pk>, <int:section>/', ODAsRedirect.as_view(), name='go_back_view'),
-    path('odas_dashboard/', ODADashboardView.as_view(), name='oda_dashboard_view'),
-    path('odas_update/<int:pk>', ODAUpdateView.as_view(), name='odas_update_view'),
-    path('odas_create/', ODACreateView.as_view(), name='odas_create_view'),
+    path('section/<int:pk>,<int:section>/', ODAsSectionView.as_view(), name='odas_section_view'),
+    path('position/<int:pk>,<int:section>/', ODAsPositionView.as_view(), name='odas_position_view'),
+    path('preview/<int:pk>/', ODAsPreviewView.as_view(), name='odas_preview_view'),
+    path('back/<slug:view>, <int:pk>, <int:section>/', ODAsRedirect.as_view(), name='go_back_view'),
+    path('dashboard/', ODADashboardView.as_view(), name='oda_dashboard_view'),
+    path('update/<int:pk>', ODAUpdateView.as_view(), name='odas_update_view'),
+    path('create/', ODACreateView.as_view(), name='odas_create_view'),
 ]
