1e92dc70f3f2bca268a6cb8bb4e3d89ce0440e82
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-06-01 18:39:48 -0500

    moments_views.py added

---
+++ studio/views/moment_views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+
+from alumnica_model.models import Moment
+
+
+class MomentsView(LoginRequiredMixin, ListView):
+    login_url = 'login_view'
+    template_name = 'studio/dashboard/ambitos.html'
+    queryset = Moment.objects.all()
+    context_object_name = 'moments_list'
