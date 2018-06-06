7ed1d07f4dd521971bbe8f30548b3d6e4a9d8014
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-06-06 13:33:03 -0500

    Implements ambit preview for supervisor
    Preview view implemented for supervisor with user type validation on dispatch method

--- studio/views/supervisor_views.py
+++ studio/views/supervisor_views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
 
-from alumnica_model.models import Ambit
+from alumnica_model.models import Ambit, users
 
 
 class AproveToPublishDashboard(LoginRequiredMixin, ListView):
@@ -9,3 +10,26 @@ class AproveToPublishDashboard(LoginRequiredMixin, ListView):
     template_name = ""
     queryset = Ambit.objects.filter(is_draft=False, is_published=False)
     context_object_name = "ambits_2_aprove_list"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.type != users.TYPE_SUPERVISOR:
+            return redirect(to="dashboard_view")
+        else:
+            return super(AproveToPublishDashboard, self).dispatch(self, request, *args, **kwargs)
+
+
+class AmbitPreviewView(LoginRequiredMixin, DetailView):
+    login_url = "login_view"
+    template_name = ""
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.type != users.TYPE_SUPERVISOR:
+            return redirect(to="dashboard_view")
+        else:
+            return super(AmbitPreviewView, self).dispatch(self, request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(AmbitPreviewView, self).get_context_data(**kwargs)
+        ambit = Ambit.objects.get(pk=kwargs['pk'])
+        context.update({'ambit': ambit})
+        return context
