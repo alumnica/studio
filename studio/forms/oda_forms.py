f7896f3eb05ad2b303eddb7657fcd37103f5035b
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-06-19 17:09:48 -0500

    Uploads evaluation file and creates questions
    Implemented method to create questions in create oda form from an excel file

--- studio/forms/oda_forms.py
+++ studio/forms/oda_forms.py
@@ -1,9 +1,10 @@
 import os
-
+import xlrd
+import json
 from django import forms
 from alumnica_model.models import ODA
 from alumnica_model.models.content import Subject, Tag, Moment, MicroODA, Image, Evaluation
-
+from alumnica_model.models.questions import *
 
 
 class ODAsSectionForm(forms.ModelForm):
@@ -106,13 +107,47 @@ class ODACreateForm(forms.ModelForm):
             oda.evaluation = Evaluation.objects.get(name=evaluation)
 
         if evaluation_file is not None:
-            pass
-
+            evaluation_object = self.set_evaluation(oda, evaluation_file)
+            oda.evaluation = evaluation_object
         oda.save()
         return oda
 
-    def set_evaluation(self, file):
-        pass
+    def set_evaluation(self, oda, file):
+        evaluation = Evaluation.objects.create(name='{}_evaluation'.format(oda.name), file=file, file_name=file.name)
+        file.seek(0)
+        file_read = file.read()
+        relationship_questions = get_json_from_excel(file_read, 0)
+        for question_data in relationship_questions:
+            question = RelationShipQuestion.objects.create(questions=question_data['Preguntas'],
+                                                           answers=question_data['Respuestas'], evaluation=evaluation)
+
+        multiple_option_questions = get_json_from_excel(file_read, 1)
+        for question_data in multiple_option_questions:
+            question = MultipleOptionQuestion.objects.create(question=question_data['Pregunta'],
+                                                             correct_answer=question_data['RespuestaOK'],
+                                                             incorrect_answers=question_data['RespuestasNOK'],
+                                                             evaluation=evaluation)
+
+        multiple_answer_questions = get_json_from_excel(file_read, 2)
+        for question_data in multiple_answer_questions:
+            question = MultipleAnswerQuestion.objects.create(question=question_data['Pregunta'],
+                                                             correct_answers=question_data['RespuestasOK'],
+                                                             incorrect_answers=question_data['RespuestasNOK'],
+                                                             evaluation=evaluation)
+
+        numeric_questions = get_json_from_excel(file_read, 3)
+        for question_data in numeric_questions:
+            question = NumericQuestion.objects.create(question=question_data['Pregunta'],
+                                                      min_limit=question_data['LimiteMenor'],
+                                                      max_limit=question_data['LimiteMayor'],
+                                                      evaluation=evaluation)
+
+        pull_down_list_questions = get_json_from_excel(file_read, 4)
+        for question_data in pull_down_list_questions:
+            question = PullDownListQuestion.objects.create(questions=question_data['Preguntas'],
+                                                           answers=question_data['Respuestas'], evaluation=evaluation)
+
+        return evaluation
 
 
 class ODAUpdateForm(forms.ModelForm):
@@ -202,3 +237,21 @@ class ODAUpdateForm(forms.ModelForm):
         oda.save()
 
         return oda
+
+
+def get_json_from_excel(file, sheet_name):
+    workbook = xlrd.open_workbook(file_contents=file)
+    worksheet = workbook.sheet_by_index(sheet_name)
+
+    data = []
+    keys = [v.value for v in worksheet.row(0)]
+    for row_number in range(worksheet.nrows):
+        if row_number == 0:
+            continue
+        row_data = {}
+        for col_number, cell in enumerate(worksheet.row(row_number)):
+            row_data[keys[col_number]] = cell.value
+        data.append(row_data)
+
+    json_output = json.loads(json.dumps(data))
+    return json_output
