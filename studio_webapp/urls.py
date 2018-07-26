192d76423be57bb9cbbade78d0a3fda4257f623c
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-07-26 16:20:07 -0500

    Adds django_h5p api_viewsets urls

--- studio_webapp/urls.py
+++ studio_webapp/urls.py
@@ -20,6 +20,10 @@ from django.urls import path, include
 from rest_framework import routers
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
+
+from django_h5p.api_viewsets import H5PackageViewSet, H5PLibraryViewSet, H5PackageDependencyViewSet, \
+    PreloadedCSSViewSet, PreloadedJSViewSet, EmbedTypeViewSet, PackageUploadAPIView, JobAPIView
+from django_h5p.views import ThreeSixtyView
 from studio import api_viewsets
 from studio.views.user_views import IndexView
 from django.views.i18n import JavaScriptCatalog
@@ -27,6 +31,12 @@ from django.views.i18n import JavaScriptCatalog
 router = routers.DefaultRouter()
 router.register(r'images', api_viewsets.ImageViewSet)
 router.register(r'evaluations', api_viewsets.EvaluationViewSet)
+router.register(r'h5packages', H5PackageViewSet)
+router.register(r'h5p_libraries', H5PLibraryViewSet)
+router.register(r'h5package_dependencies', H5PackageDependencyViewSet)
+router.register(r'preloaded_css', PreloadedCSSViewSet)
+router.register(r'preloaded_js', PreloadedJSViewSet)
+router.register(r'embed_types', EmbedTypeViewSet)
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -44,5 +54,8 @@ urlpatterns += i18n_patterns(
                   path(_('admin/'), admin.site.urls),
                   path(_('api/'), include(router.urls)),
                   path(_('api-auth/'), include('rest_framework.urls', namespace='rest_framework')),
+                  path('360/', ThreeSixtyView.as_view()),
+                  path('api/zip_files/', PackageUploadAPIView.as_view()),
+                  path('api/jobs/<str:job_id>/', JobAPIView.as_view(), name='job_detail_view'),
                   path(_('jsi18n/'), JavaScriptCatalog.as_view(), name='javascript-catalog'),
               )
